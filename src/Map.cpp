39e1003445644acba8518100de2037d295c93569
erickWasTaken <gabriex86@gmail.com>
2025-09-12 10:14:51 +0900

    Collecting active tiles

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,24 +1,52 @@
 #include "Map.hpp"
 #include <iostream>
 #include <SFML/System/Vector2.hpp>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+vector<uint8_t> map;
+vector<int> active;
+
+void Map::Test(){
+    for(int i = 0; i < active.size(); i++){
+        map[active[i]] = 127;
+    }
+
+    cout << active.size() << endl;
+
+    for(int i = 0; i < COLS * ROWS; i++){
+        cout << (int)map[i] << "    ";
+
+        if(i % COLS == 0)
+            cout << endl;
+    }
+}
+
+void Map::Load(){
+    for(int i = 0; i < COLS * ROWS; i++){
+        uint8_t cell = (uint8_t)(layout.getPixel(sf::Vector2u(i % COLS, i / COLS)).a);
+        map.push_back(cell);
+        if(cell != 0)
+            active.push_back(i);
+    } 
+
+    Test();
+}
+
+void Map::Draw(sf::RenderTexture target){
+     
+}
 
 Map::Map(const char* atlasPath){
     layout = sf::Image(atlasPath);
     atlas = sf::Texture(atlasPath);
     COLS = layout.getSize().x;
     ROWS = layout.getSize().y;
+    TILESIZE = 16;
     std::cout << "Map dimentions: " << COLS << ", " << ROWS << "\n" << std::endl;
 
-    Draw();
-}
-
-void Map::Draw(){
-    for(int i = 0; i < COLS; i++){
-        for(int j = 0; j < ROWS; j++){
-            bool on = layout.getPixel(sf::Vector2u(i, j)).a != 0;
-            char cell = on ? '#' : ' ';
-            std::cout << cell << "  ";
-        }
-        std::cout << std::endl;
-    } 
+    Load();
 }
