fcb87cbe745711bfa6409c329d8672c4eadde323
erickWasTaken <gabriex86@gmail.com>
2025-09-21 05:31:00 +0900

    file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
 #include "DrawWindow.hpp"
+#include "FileUtil.hpp"
 
 using namespace std;
 
 int main(){
     cout << "battlenet - remake by erickWasTaken" << endl;
     DrawWindow win;
-    win.Initialize("", DrawWindow::WindowMode::fullscreen);
+    win.Initialize("", DrawWindow::WindowMode::windowed);
+
+    string path("filetest.txt");
+    cout << "::READING FILE:: \n" << FileUtil::Read(path) << endl;
 
     while(win.isOpen()){
         win.clear();
